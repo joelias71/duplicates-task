ba5d8421b68220fd3ffeaf1244280ec801669e60
Jorge Elias <jorgea.eliasl7@gmail.com>
2025-03-03 13:41:01 -0600

    test: Update tests using parametrized test

--- src/test/java/org/example/UtilTest.java
+++ src/test/java/org/example/UtilTest.java
@@ -1,29 +1,26 @@
 package org.example;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 class UtilTest {
-    @Test
-    void removeDuplicates() {
-        String[] inputs = {
-                "AABBCCD112233",
-                "cbacdcbc",
-                "aAbDDBD4477123",
-                "1232243890"
-        };
-        String[] expectedOutput = {
-                "ABCD123",
-                "cbad",
-                "aAbDB47123",
-                "1234890"
-        };
+    @ParameterizedTest
+    @MethodSource("inputStrings")
+    void removeDuplicates(String input, String expectedOutput) {
+        String result = Util.removeDuplicates(input);
+        Assertions.assertEquals(result, expectedOutput);
+    }
 
-        int index = 0;
-        for (String input : inputs) {
-           String result = Util.removeDuplicates(input);
-           Assertions.assertEquals(result, expectedOutput[index]);
-           index++;
-        }
+    private static Stream<Arguments> inputStrings() {
+        return Stream.of(
+                Arguments.of("AABBCCD112233", "ABCD123"),
+                Arguments.of("cbacdcbc", "cbad"),
+                Arguments.of("aAbDDBD4477123", "aAbDB47123"),
+                Arguments.of("1232243890", "1234890")
+        );
     }
 }
