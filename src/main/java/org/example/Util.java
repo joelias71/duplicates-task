2a80783bb9a47d2a9cf8d1d13d6c8d518506f723
Jorge Elias <jorgea.eliasl7@gmail.com>
2025-03-03 18:16:31 -0600

    fix: Add validation if input is null

--- src/main/java/org/example/Util.java
+++ src/main/java/org/example/Util.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 public class Util {
     public static String removeDuplicates(String string) {
+        if (string == null || string.isEmpty()) return "";
         return Arrays.asList(string.split(""))
                 .stream()
                 .distinct()
